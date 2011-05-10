ca54940869e70bcb07ffb64a712c8e104bfc5b5e
Maxious <maxious@lambdacomplex.org>
2011-05-10 18:57:54 +1000

    Lock down database infrastructure

--- include/common-db.inc.php
+++ include/common-db.inc.php
@@ -1,13 +1,15 @@
 <?php
   if (php_uname('n') == "actbus-www") {
     $conn = pg_connect("dbname=transitdata user=transitdata password=transitdata host=bus-main.lambdacomplex.org");
-  } else if (isDebugServer()) {
+  } 
+  if (isDebugServer()) {
     $conn = pg_connect("dbname=transitdata user=postgres password=snmc");
-  } else {
+  } 
+  if (strstr(php_uname('n'),"ip-10")){
     $conn = pg_connect("dbname=transitdata user=transitdata password=transitdata ");
   }
   if (!$conn) {
-      die("A database error occurred.\n");
+      die("A database error occurred on ".php_uname('n')."\n");
   }
   
   function databaseError($errMsg) {
