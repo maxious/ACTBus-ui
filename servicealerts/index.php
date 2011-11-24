ac5bf725c99dc76775d7debaa0e89d340392fe0a
Alexander Sadleir <maxious@lambdacomplex.org>
2011-11-25 00:32:19 +1100

    Add service alert viewer for all users

--- servicealerts/index.php
+++ servicealerts/index.php
@@ -2,12 +2,17 @@
 include ('../include/common.inc.php');
 
 include_header("Service Alerts", "index");
+echo '<ul data-role="listview" data-theme="e" data-groupingtheme="e">';
+	
  if ($_SESSION['authed'] == true) {
- 	echo '<ul data-role="listview" data-theme="e" data-groupingtheme="e">
-		<li><a href="servicealert_editor.php"><h3>servicealert_editor</h3>
-		<p>servicealert_editor</p></a></li>
-            </ul>';
- }
+ 	echo'	<li><a href="servicealert_editor.php"><h3>servicealert_editor</h3>
+		<p>servicealert_editor</p></a></li>';
+          }
+ 	echo'	<li><a href="servicealert_viewer.php"><h3>Service Alert Viewer</h3>
+		<p>Browse current network alerts</p></a></li>';
+          
+           echo'  </ul>';
+
 ?>	    </div>
 <?php
 include_footer()
