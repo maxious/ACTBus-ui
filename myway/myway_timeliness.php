c1a0400e75494cad067f8c678d839a828291dd25
Maxious <maxious@lambdacomplex.org>
2012-08-16 09:48:58 +1000

    Bubble graph for timeliness

--- myway/myway_timeliness.php
+++ myway/myway_timeliness.php
@@ -20,95 +20,83 @@ include_header("MyWay Deltas", "mywayDelta");
 <!--[if lt IE 9]>
     <script type="text/javascript" src="../js/FlashCanvas/bin/flashcanvas.js"></script>
     <![endif]-->
-    <script type="text/javascript" src="../js/flotr2/flotr2.min.js"></script>
-<center><div id="placeholder" style="width:900px;height:550px"></div></center>
-<script type="text/javascript"> 
+<script type="text/javascript" src="../js/flotr2/flotr2.min.js"></script>
+<center>
+    <div id="placeholder" style="width:900px;height:550px"></div>
+</center>
+<script type="text/javascript">
     $(function () {
         var d = new Date();
         d.setUTCMinutes(0);
         d.setUTCHours(0);
         var midnight = d.getTime();
 
-<?php
-$query = "select * from myway_timingdeltas where abs(timing_delta) < 2*(select stddev(timing_delta) from myway_timingdeltas) order by route_name;";
-$query = $conn->prepare($query);
-$query->execute();
-if (!$query) {
-    databaseError($conn->errorInfo());
-    return Array();
-}
-$i = 0;
-$labels = Array();
-$lastRoute = "";
-foreach ($query->fetchAll() as $delta) {
-      /*$routeIDParts = explode(" ",$delta['route_name']);
-    $routeNumber = $routeIDParts[0];
-    $routeDirection = $routeIDParts[1];
-    if (preg_match('/31./',$routeName)) {
-        $routeName = "312-319"." ".$routeDirection;
-    } else {
-        $routeName = $delta['route_name'];
-    }*/
-    
-    $routeName = $delta['route_name'];
-    
-    if (preg_match('/31./',$routeName)) {
-        $routeName = "312-319";
-    } else {
-        $routeName = preg_replace('/\D/', '', $routeName);
+    <?php
+    /*CREATE OR REPLACE FUNCTION round_time(TIMESTAMP WITH TIME ZONE)
+  RETURNS TIMESTAMP WITH TIME ZONE AS $$
+    SELECT date_trunc('hour', $1) + INTERVAL '15 min' * ROUND(date_part('minute', $1) / 15.0)
+  $$ LANGUAGE SQL;*/
+    $query = "
+
+select round_time(current_date + time) as timer, timing_delta, count(*) as count from myway_timingdeltas where abs(timing_delta) < 2*(select stddev(timing_delta) from myway_timingdeltas)
+group by time, timing_delta order by time;";
+
+    $query = $conn->prepare($query);
+    $query->execute();
+    if (!$query) {
+        databaseError($conn->errorInfo());
+        return Array();
     }
-    if ($routeName != $lastRoute) {
-        $i++;
-        echo "    var d$i = [];";
-        $lastRoute = $routeName;
-        $labels[$i] = $routeName;
-    }
-    echo "d$i.push([ midnight+ (1000*" . midnight_seconds(strtotime($delta['time'])) . "), " . intval($delta['timing_delta']) . "]); \n";
-};
-?>
+    $i = 0;
+    $labels = Array();
+    $lastRoute = "";
+
+    echo "    var d1 = [];";
+    foreach ($query->fetchAll() as $delta) {
+        echo "d1.push([ midnight+ (1000*" . midnight_seconds(strtotime($delta['timer'])) . "), " . intval($delta['timing_delta']) . "," . $delta['count'] . "]); \n";
+    };
+    ?>
 
         var placeholder = document.getElementById("placeholder");
 
         var plot = Flotr.draw(placeholder, [
-<?php
-foreach ($labels as $key => $label) {
-    echo "        {
-            data: d$key,
-            points: { show: true },
-            label: '$label'
+        <?php
+        echo "        {
+            data: d1,
+            label: 'Timing Deltas'
         },";
-}
-?>
+        ?>
         ],
-        {
-            xaxis: {
-                mode: "time",
-                 min: midnight + (1000*60*60*5.6),
-                max: midnight + (1000*60*60*23.5)
+            {
+                bubbles : { show : true, baseRadius : 5 },
+                xaxis:{
+                    mode:"time",
+                    min:midnight + (1000 * 60 * 60 * 5.6),
+                    max:midnight + (1000 * 60 * 60 * 23.5)
 
-            },
-            yaxis: {
-                tickFormatter: yformatter,
-                min: -60*10,
-                max: 60*10
-            },
-            mouse: { track: true, relative: true, trackFormatter: showTooltip}
-        });
-       
+                },
+                yaxis:{
+                    tickFormatter:yformatter,
+                    min:-60 * 10,
+                    max:60 * 10
+                },
+                mouse:{ track:true, relative:true, trackFormatter:showTooltip}
+            });
+
     });
     function yformatter(v) {
-        if (Math.floor(v/60) < -9) return "";
-        return Math.abs(Math.floor(v/60)) + " min " + (v == 0 ? "" : (v >0 ? "early":"late"))
+        if (Math.floor(v / 60) < -9) return "";
+        return Math.abs(Math.floor(v / 60)) + " min " + (v == 0 ? "" : (v > 0 ? "early" : "late"))
     }
     function showTooltip(point) {
 
-                    
-                var d = new Date();
-                d.setTime(point.x);
-                var time = d.getUTCHours() +':'+ (d.getUTCMinutes().toString().length == 1 ? '0'+ d.getMinutes():  d.getUTCMinutes())
 
-                    
-                return  point.series.label + " at "+ time +" = " + Math.abs(new Number(point.y/60).toFixed(2))+" minutes "+(point.y >0 ? "early":"late");
+        var d = new Date();
+        d.setTime(point.x);
+        var time = d.getUTCHours() + ':' + (d.getUTCMinutes().toString().length == 1 ? '0' + d.getMinutes() : d.getUTCMinutes())
+
+
+        return  point.series.label + " at " + time + " = " + Math.abs(new Number(point.y / 60).toFixed(2)) + " minutes " + (point.y > 0 ? "early" : "late");
     }
 
 </script> 
